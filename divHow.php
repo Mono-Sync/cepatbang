938f4608c589fa13750bcaf0fbdb0e36aa431160
Edwin Capel <edwincapel@Edwins-MacBook-Pro.local>
2017-11-08 03:43:46 +0800

    added how it works section

---
+++ divHow.php
@@ -0,0 +1,24 @@
+<div class="clear">
+
+</div>
+<div class="container">
+      <div class="row" >
+
+        <div class="col-md-8 offset-md-2">
+          <h3 id="howWork">How it works</h3>
+        </div>
+      </div>
+      <div class="clear">
+
+      </div>
+      <div class="row" >
+
+        <div class="col-md-8 offset-md-2 pic">
+          <img src="img/how.png" alt="">
+        </div>
+      </div>
+    </div>
+
+    <div class="clear">
+
+    </div>
