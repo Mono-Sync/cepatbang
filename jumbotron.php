654613248f3536e2a326990754542af548210b7b
Edwin Capel <edwincapel@Edwins-MacBook-Pro.local>
2017-11-08 02:34:37 +0800

    added base project

---
+++ jumbotron.php
@@ -0,0 +1,16 @@
+<!-- Main jumbotron for a primary marketing message or call to action -->
+<div class="jumbotron">
+  <div class="containerForm" id="space">
+    <form class="form-inline" >
+
+    <label class="sr-only" for="inlineFormInputGroup">PostCode</label>
+    <div class="input-group input-group-lg">
+    <div class="input-group-addon postcode"><span class="fa fa-map-marker fa-lg" aria-hidden="true"></span></div>
+    <input type="text" class="resizedTextBox" id="inlineFormInputGroup" placeholder="&nbsp;&nbsp;Enter your address">
+    </div>
+
+    &nbsp;&nbsp;<button type="submit" id="button1" class="btn btn-primary">Delivery</button> &nbsp;&nbsp;
+    <button type="submit" id="button2" class="btn btn-primary">Pick-up</button>
+    </form>
+</div>
+</div>
