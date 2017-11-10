67aba7b18862a85bff5d55731afb85e9cee5c983
Edwin Capel <edwincapel@Edwins-MacBook-Pro.local>
2017-11-10 20:33:34 +0800

    added first name last name feild

--- modal.php
+++ modal.php
@@ -16,16 +16,18 @@
       </ul>
       </div>
   <div class="cont_text_inputs">
-      <input type="text" class="input_form_sign " placeholder="NAME" name="name_us" />
+      <input type="text" class="input_form_sign " placeholder="FIRST NAME" name="firstname" />
 
-    <input type="text" class="input_form_sign d_block active_inp" placeholder="EMAIL" name="emauil_us" />
+      <input type="text" class="input_form_sign " placeholder="LAST NAME" name="lastname" />
 
-    <input type="password" class="input_form_sign d_block  active_inp" placeholder="PASSWORD" name="pass_us" />
+    <input type="text" class="input_form_sign d_block active_inp" placeholder="EMAIL" name="email" />
+
+    <input type="password" class="input_form_sign d_block  active_inp" placeholder="PASSWORD" name="password" />
    <input type="password" class="input_form_sign" placeholder="CONFIRM PASSWORD" name="conf_pass_us" />
 
     <a href="#" class="link_forgot_pass d_block" >Forgot Password ?</a>
 <div class="terms_and_cons d_none">
-    <p><input type="checkbox" name="terms_and_cons" /> <label for="terms_and_cons">Accept  Terms and Conditions.</label></p>
+    <p><input type="checkbox" name="terms_and_cons" /> <label for="terms_and_cons">Accept Terms and Conditions.</label></p>
 
     </div>
       </div>
